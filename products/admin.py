9f61b0067ce74b78e7abb9109b011b3396539016
MohammadaliJafari <m.alijafari96@gmail.com>
2025-04-27 04:37:01 +0330

    Add Products Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
+from .models import Product, Category, Comment, Question, Answer, Image, ProductOption, ProductPrice
+
 
 # Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'english_name', 'name', 'category']
+    list_filter = ['category']
+    search_fields = ['name', 'english_name']
+    inlines = [ProductImageInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'parent']
+    list_filter = ['parent']
+    search_fields = ['name', 'description']
+    fieldsets = [
+        ("Details", {'fields': ['name', 'slug', 'parent', 'description']}),
+        ("Image", {'fields': ['icon', 'image']}),
+    ]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_email', 'product', 'title', 'rate']
+    list_filter = ['product']
+    search_fields = ['email', 'product']
+    fieldsets = [
+        ("Details", {'fields': ['title', 'text', 'rate']}),
+        ("Product", {'fields': ['product']}),
+        ("Writer", {'fields': ['user_email']}),
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_email', 'product', 'text']
+    list_filter = ['product', 'user_email']
+    search_fields = ['user_email', 'product']
+    fieldsets = [
+        ("Question", {'fields': ['text']}),
+        ("Product", {'fields': ['product']}),
+        ("Writer", {'fields': ['user_email']}),
+    ]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'text']
+    list_filter = ['question']
+    search_fields = ['question']
+    fieldsets = [
+        ("Answer", {'fields': ['text']}),
+        ("Question", {'fields': ['question']}),
+    ]
+
+
+@admin.register(ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'name', 'value']
+    list_filter = ['product', 'name']
+    search_fields = ['name', 'value', 'product']
+    fieldsets = [
+        ("Product", {'fields': ['product']}),
+        ("Option", {'fields': ['name', 'value']}),
+    ]
+
+
+@admin.register(ProductPrice)
+class ProductPriceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'price', 'create_at', 'update_at']
+    list_filter = ['product']
+    search_fields = ['product']
+    fieldsets = [
+        ("Product", {'fields': ['product']}),
+        ("Price", {'fields': ['price']}),
+    ]
+
