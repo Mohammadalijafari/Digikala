ec0a029519a2bf1ebebfe1af41761bb7712a26c4
MohammadaliJafari <m.alijafari96@gmail.com>
2025-05-01 22:07:15 +0330

    Update views and urls of the products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Category, Product
+from django.shortcuts import HttpResponse, get_object_or_404, render
+
 
 # Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()[:10]
+    return HttpResponse()
+
+
+def product_view(request, product_id):
+    p = get_object_or_404(Product, id=product_id)
+    context = {'product': p}
+    return render(
+        request=request,
+        template_name="products/product.html",
+        context=context
+    )
