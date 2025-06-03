11ca887e8dcd0f62e773c4da9312768c475194a8
MohammadaliJafari <m.alijafari96@gmail.com>
2025-06-03 09:42:34 +0330

    Add register and profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accounts.views import login_view
+from accounts.views import login_view, user_register_view, user_info_view, user_logout_view
 
 app_name = "accounts"
 
 urlpatterns = [
     path('login/', login_view, name='login_view'),
+    path('register/', user_register_view, name='register_view'),
+    path('profile/', user_info_view, name='user_info_view'),
+    path('logout/', user_logout_view, name='user_logout_view'),
 ]
