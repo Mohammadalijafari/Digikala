f0d5ffe022c84a98f0aec106ecd9e049ab6abdcc
MohammadaliJafari <m.alijafari96@gmail.com>
2025-06-03 09:42:34 +0330

    Add register and profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate
+from .models import User
 
 
 class UserLoginForm(forms.Form):
@@ -25,3 +26,40 @@ class UserLoginForm(forms.Form):
             return clean_data
         else:
             raise forms.ValidationError('credential is invalid')
+
+
+class UserRegisterFrom(forms.ModelForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput({"class": "form-control"}), required=True, label="کلمه عبور"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput({"class": "form-control"}), required=True, label="تکرار کلمه عبور"
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'mobile'
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.pop("password1", None)
+        password2 = cleaned_data.pop("password2", None)
+        if password1 and password2 and password1 != password2:
+            self.add_error(
+                'password2',
+                forms.ValidationError('در وارد کردن کلمه عبور دقت کنید', code='password_mismatch'))
+        cleaned_data.setdefault('password', password1)
+        return cleaned_data
+
+    def save(self, commit: bool = ...):
+        user = super().save(commit)
+        user.setPassword(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
